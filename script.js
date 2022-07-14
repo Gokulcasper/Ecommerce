3e67cf23df7f68aae7b35230ccce2834be6babe4
Gokulcasper <ronitcasper215@gmail.com>
2022-07-14 15:14:55 +0530

    Feat : Ecommerce Landing Page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const wrapper = document.querySelector(".sliderWrapper");
-const menuItem = document.querySelectorAll(".menuItem");
+const menuItems = document.querySelectorAll(".menuItem");
 
 const products = [
   {
@@ -78,9 +78,47 @@ const products = [
     ],
   },
 ];
+let choosenProduct = products[0];
 
-menuItem.forEach((item, index) => {
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
-    wrapper.style.transform = `translate(${-100 * index}vw)`;
+    //change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index];
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "$" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size, index) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
   });
 });
